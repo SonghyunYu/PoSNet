09432a22a849b8dcb5c01dc92a65d5c1f9371237
SonghyunYu <3069song@naver.com>
2019-09-20 15:49:31 +0900

    minor edit

--- prepare_data.py
+++ prepare_data.py
@@ -2,66 +2,60 @@ import os
 import h5py
 import numpy as np
 from PIL import Image
-import torch
-from torch.utils.data import Dataset, DataLoader
-from torchvision.transforms import transforms
 import glob
 
-data_path = 'D:/ptorch/projects/Temporal_Video_SR/data/train15/one_folder'
+data_path = 'D:/ptorch/projects/Temporal_Video_SR/data/val_one_folder'
 files = glob.glob(os.path.join(data_path, '*.png'))
 num_img = len(files)
 
-Previous = []
-Current = []
-ct = 0
-for file in files:
-    ct += 1
-    im = np.array(Image.open(file)).transpose(2, 0, 1)
-    if ct % 46 != 0:
-        for i in range(4):
-            for j in range(4):
-                patch = im[:, 160*i:160*(i+1), 320*j:320*(j+1)]
-                Previous.append(patch)
+
+Previous = np.zeros((2670, 3, 720, 1280), dtype=np.uint8)
+gt1 = np.zeros((2670, 3, 720, 1280), dtype=np.uint8)
+gt2 = np.zeros((2670, 3, 720, 1280), dtype=np.uint8)
+gt3 = np.zeros((2670, 3, 720, 1280), dtype=np.uint8)
+Current = np.zeros((2670, 3, 720, 1280), dtype=np.uint8)
+
+c1, c2, c3, c4, c5 = 0, 0, 0, 0, 0
+for seq in range(num_img):
+    ct = (seq+1) % 181
+
+    if ct % 4 == 1 or ct % 4 == 3 and ct < 178:
+        im = np.array(Image.open(files[seq])).transpose(2, 0, 1)
+        Previous[c1] = im
+
+        im = np.array(Image.open(files[seq+1])).transpose(2, 0, 1)
+        gt1[c1] = im
+        im = np.array(Image.open(files[seq+2])).transpose(2, 0, 1)
+        gt2[c1] = im
+        im = np.array(Image.open(files[seq+3])).transpose(2, 0, 1)
+        gt3[c1] = im
+
+        im = np.array(Image.open(files[seq+4])).transpose(2, 0, 1)
+        Current[c1] = im
+
+        c1 += 1
 
 
-    #if ct % 46 != 1:
-    #    for i in range(4):
-    #       for j in range(4):
-    #           patch = im[:, 160*i:160*(i+1), 320*j:320*(j+1)]
-    #           Current.append(patch)
 
-    if ct % 100 == 0:
-        print(100*ct/num_img, "percent done")
+    if seq % 100 == 0:
+        print(100*seq/num_img, "percent done")
 
-
-Previous = np.uint8(np.array(Previous))
-with h5py.File('./data/15fps_previous_patch160x320.h5', 'w') as h:
+print(c1)
+with h5py.File('./data/15fps3_previous_frame.h5', 'w') as h:
     h.create_dataset('data', data=Previous, shape=Previous.shape)
 del Previous
-
-#Current = np.uint8(np.array(Current))
-#with h5py.File('./data/15fps_current_patch160x320.h5', 'w') as h:
-#    h.create_dataset('data', data=Current, shape=Current.shape)
-#del Current
-
-
-
-# 기존에 있던 파일에서 데이터 바꾸기. -> shape 같아야됨.
-# f1 = h5py.File(file_name, 'r+')     # open the file
-# data = f1['meas/frame1/data']       # load the data
-# data[...] = X1                      # assign new values to data
-# f1.close()                          # close the file
-
-# 기존에 있던 파일에서 데이터 지우고 새로 쓰기. -> shape 달라도 됨
-# del f1['meas/frame1/data']
-# dset = f1.create_dataset('meas/frame1/data', data=X1)
-
-
-# 예상 응용
-# f1 = h5py.File(file_name, 'r+')     # open the file
-# data = f1['meas/frame1/data']       # load the data
-# data[...] = X1                      # assign new values to data
-# f1.close()                          # close the file
+with h5py.File('./data/15fps3_gt1_frame.h5', 'w') as h:
+    h.create_dataset('label', data=gt1, shape=gt1.shape)
+del gt1
+with h5py.File('./data/15fps3_gt2_frame.h5', 'w') as h:
+    h.create_dataset('label', data=gt2, shape=gt2.shape)
+del gt2
+with h5py.File('./data/15fps3_gt3_frame.h5', 'w') as h:
+    h.create_dataset('label', data=gt3, shape=gt3.shape)
+del gt3
+with h5py.File('./data/15fps3_current_frame.h5', 'w') as h:
+    h.create_dataset('data', data=Current, shape=Current.shape)
+del Current
 
 
 
